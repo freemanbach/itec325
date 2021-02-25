e9c82b3128bbb0f6d5b71066d536cf5a13527966
jakobntc <jclapsaddle@radford.edu>
2021-02-25 00:51:52 -0500

    Linked to the photo "ginger.jpeg" in the UL

--- picts.php
+++ picts.php
@@ -35,6 +35,9 @@ require_once('utils.php');
       <li>
 	<?php echo thumbnail("eggplant-man-statue.jpg",300); ?> Beware, the eggplant man!
       </li>
+      <li>
+	<?php echo thumbnail("ginger.jpeg",300); ?> My dog Ginger!
+      </li>
     </ul>
 
   </body>
